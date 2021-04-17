754904da77ed6b58fbcc42bf3ed01d65acd57f87
TheSpecialCoder <55451896+visheshdvivedi@users.noreply.github.com>
2021-04-17 14:46:30 +0530

    Update tic_tac_toe.c

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[3][3] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'}
+};
 
 int check();
 void chessboard();
+void printWelcomeMessage();
 
 int main()
 {
@@ -12,6 +17,8 @@ int main()
     char mark;
     do
     {
+        system("clear");
+        printWelcomeMessage();
         chessboard();
         player = (player % 2) ? 1 : 2;
 
@@ -20,37 +27,37 @@ int main()
 
         mark = (player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1] == '1')
-            square[1] = mark;
+        if (choice == 1 && square[0][0] == '1')
+            square[0][0] = mark;
             
-        else if (choice == 2 && square[2] == '2')
-            square[2] = mark;
+        else if (choice == 2 && square[0][1] == '2')
+            square[0][1] = mark;
             
-        else if (choice == 3 && square[3] == '3')
-            square[3] = mark;
+        else if (choice == 3 && square[0][2] == '3')
+            square[0][2] = mark;
             
-        else if (choice == 4 && square[4] == '4')
-            square[4] = mark;
+        else if (choice == 4 && square[1][0] == '4')
+            square[1][0] = mark;
             
-        else if (choice == 5 && square[5] == '5')
-            square[5] = mark;
+        else if (choice == 5 && square[1][1] == '5')
+            square[1][1] = mark;
             
-        else if (choice == 6 && square[6] == '6')
-            square[6] = mark;
+        else if (choice == 6 && square[1][2] == '6')
+            square[1][2] = mark;
             
-        else if (choice == 7 && square[7] == '7')
-            square[7] = mark;
+        else if (choice == 7 && square[2][0] == '7')
+            square[2][0] = mark;
             
-        else if (choice == 8 && square[8] == '8')
-            square[8] = mark;
+        else if (choice == 8 && square[2][1] == '8')
+            square[2][1] = mark;
             
-        else if (choice == 9 && square[9] == '9')
-            square[9] = mark;
+        else if (choice == 9 && square[2][2] == '9')
+            square[2][2] = mark;
             
         else
         {
-            printf("Invalid move ");
-
+            printf("Invalid move \n");
+            sleep(2);
             player--;
          
         }
@@ -80,33 +87,33 @@ O GAME IS OVER AND NO RESULT
 
 int check()
 {
-    if (square[1] == square[2] && square[2] == square[3])
+    if (square[0][0] == square[0][1] && square[0][1] == square[0][2])
         return 1;
         
-    else if (square[4] == square[5] && square[5] == square[6])
+    else if (square[1][0] == square[1][1] && square[1][1] == square[1][2])
         return 1;
         
-    else if (square[7] == square[8] && square[8] == square[9])
+    else if (square[2][0] == square[2][1] && square[2][1] == square[2][2])
         return 1;
         
-    else if (square[1] == square[4] && square[4] == square[7])
+    else if (square[0][0] == square[1][0] && square[1][0] == square[2][0])
         return 1;
         
-    else if (square[2] == square[5] && square[5] == square[8])
+    else if (square[0][1] == square[1][1] && square[1][1] == square[2][1])
         return 1;
         
-    else if (square[3] == square[6] && square[6] == square[9])
+    else if (square[0][2] == square[1][2] && square[1][2] == square[2][2])
         return 1;
         
-    else if (square[1] == square[5] && square[5] == square[9])
+    else if (square[0][0] == square[1][1] && square[1][1] == square[2][2])
         return 1;
         
-    else if (square[3] == square[5] && square[5] == square[7])
+    else if (square[0][2] == square[1][1] && square[1][1] == square[2][0])
         return 1;
         
-    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
-        != '7' && square[8] != '8' && square[9] != '9')
+    else if (square[0][0] != '1' && square[0][1] != '2' && square[0][2] != '3' &&
+        square[1][0] != '4' && square[1][1] != '5' && square[1][2] != '6' && square[2][0] 
+        != '7' && square[2][1] != '8' && square[2][2] != '9')
 
         return 0;
     else
@@ -121,25 +128,47 @@ FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
 
 void chessboard()
 {
-    printf("\n\n\tTic Tac Toe\n\n");
-
     printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
 
 
     printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
+    printf("  %c  |  %c  |  %c \n", square[0][0], square[0][1], square[0][2]);
 
     printf("_____|_____|_____\n");
     printf("     |     |     \n");
 
-    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
+    printf("  %c  |  %c  |  %c \n", square[1][0], square[1][1], square[1][2]);
 
     printf("_____|_____|_____\n");
     printf("     |     |     \n");
 
-    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
+    printf("  %c  |  %c  |  %c \n", square[2][0], square[2][1], square[2][2]);
 
     printf("     |     |     \n\n");
+}
+
+/*******************************************************************
+FUNCTION TO PRINT WELCOME MESSAGE ON START OF GAME
+ ********************************************************************/
+
+void printWelcomeMessage()
+{
+    printf("'########:'####::'######:::::'########::::'###:::::'######:::::'########::'#######::'########:\n");
+    printf("... ##..::. ##::'##... ##::::... ##..::::'## ##:::'##... ##::::... ##..::'##.... ##: ##.....::\n");
+    printf("::: ##::::: ##:: ##:::..:::::::: ##:::::'##:. ##:: ##:::..:::::::: ##:::: ##:::: ##: ##:::::::\n");
+    printf("::: ##::::: ##:: ##::::::::::::: ##::::'##:::. ##: ##::::::::::::: ##:::: ##:::: ##: ######:::\n");
+    printf("::: ##::::: ##:: ##::::::::::::: ##:::: #########: ##::::::::::::: ##:::: ##:::: ##: ##...::::\n");
+    printf("::: ##::::: ##:: ##::: ##::::::: ##:::: ##.... ##: ##::: ##::::::: ##:::: ##:::: ##: ##:::::::\n");
+    printf("::: ##::::'####:. ######:::::::: ##:::: ##:::: ##:. ######:::::::: ##::::. #######:: ########:\n");
+    printf(":::..:::::....:::......:::::::::..:::::..:::::..:::......:::::::::..::::::.......:::........::\n");
+    printf("\n");
+    printf("--------------------\n");
+    printf("|    CREATORS:     |\n");
+    printf("|   Apporv Gupta   |\n");
+    printf("|  Vishesh Dvivedi |\n");
+    printf("--------------------\n");
+    printf("\n");
+    
 }
 
 /*******************************************************************
